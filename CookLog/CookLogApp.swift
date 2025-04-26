1b0156f48a45093beeb1585ef1b1fa1a1b945abe
Rachael LaMassa <158332494+rachlamassa@users.noreply.github.com>
2025-04-26 17:53:08 -0400

    Initial Commit

---
+++ CookLog/CookLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookLogApp.swift
+//  CookLog
+//
+//  Created by Rachael LaMassa on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CookLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
